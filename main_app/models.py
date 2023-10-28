fa8b91d56f49f3db2913049645fdb024847a5d15
Ellie <eagyeman1234@gmail.com>
2023-10-28 12:36:19 -0400

    add model for Finch in models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Finch(models.Model):
+  name = models.CharField(max_length=100)
+  color = models.CharField(max_length=100)
+  description = models.CharField(max_length=250)
+  age = models.IntegerField()
 
+  def __str__(self):
+    return self.name
