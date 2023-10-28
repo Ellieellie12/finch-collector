ea14d6eee89be077c911dba74333bbe9d253b1de
Ellie <eagyeman1234@gmail.com>
2023-10-28 12:17:26 -0400

    add index template and view and it renders

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+class Finch:
+  def __init__(self, name, color, description, age):
+    self.name = name
+    self.color = color 
+    self.description = description
+    self.age = age
+
+finches = [
+  Finch('Maggie', 'blue', 'quiet', 5),
+  Finch('Lolo', 'pink', 'loud', 7),
+  Finch('Sue', 'black', 'quiet', 3),
+  Finch('Fancy', 'green', 'quiet', 2),
+]
+
 def home(request):
   return HttpResponse('<h1>HELLO</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def finch_index(request):
+  return render(request, 'finches/index.html', { 'finches': finches })
