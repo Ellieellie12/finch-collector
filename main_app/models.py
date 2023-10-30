9c23f157b229f6a24286447ef98bf56ed5af7cc3
Ellie <eagyeman1234@gmail.com>
2023-10-30 19:42:21 -0400

    start functionality to display date on the calendar for feedings

--- main_app/models.py
+++ main_app/models.py
@@ -21,4 +21,13 @@ class Finch(models.Model):
   
 class Feeding(models.Model):
   date = models.DateField()
-  meal = models.CharField(max_length=1)
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+    )
+  
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
