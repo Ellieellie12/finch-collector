dc2b9bf8aa55e51279222ce0af4e4635ca9be9ce
Ellie <eagyeman1234@gmail.com>
2023-10-31 12:33:40 -0400

    display view for add a toy

--- main_app/models.py
+++ main_app/models.py
@@ -18,6 +18,8 @@ class Finch(models.Model):
 
   def get_absolute_url(self):
       return reverse("finch-detail", kwargs={"finch_id": self.id})
+  def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
   
 class Feeding(models.Model):
   date = models.DateField()
@@ -34,3 +36,15 @@ class Feeding(models.Model):
   
   class Meta:
     ordering = ['-date']
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+      return self.name
+  
+  def get_absolute_url(self):
+      return reverse("toy-detail", kwargs={"pk": self.id})
+  
+  
